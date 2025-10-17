31378fb93aa79b6c96f413e57617d34846f9b311
0hj1hyeon <ojh010911@gmail.com>
2025-10-17 23:49:21 +0900

    feat: 입출력 기능 구현

---
+++ src/main/java/calculator/view/IOView.java
@@ -0,0 +1,15 @@
+package calculator.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class IOView {
+
+    public String Input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
+
+    public void Output(int result){
+        System.out.println("결과: "+result);
+    }
+}
