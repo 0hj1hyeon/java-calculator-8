267e53ae85d4daf499f3251270118bbb72cf1644
0hj1hyeon <ojh010911@gmail.com>
2025-10-19 11:14:29 +0900

    feat: 계산 구현

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,4 +1,27 @@
 package calculator.domain;
 
 public class Calculator {
+
+    private final DelimiterManager delimiterManager;
+
+    public Calculator(DelimiterManager delimiterManager) {
+
+        this.delimiterManager = delimiterManager;
+    }
+
+    public int sum(String input) {
+
+        Validator.validateFullInput(input);
+
+        String[] numberStrings = delimiterManager.split(input);
+
+        int sum = 0;
+        for (String numberString : numberStrings) {
+
+            int number = Validator.parseAndValidateNumber(numberString);
+            sum += number;
+        }
+
+        return sum;
+    }
 }
