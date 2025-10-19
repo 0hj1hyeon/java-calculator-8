73f187f9569ca5306c2213b9312daccffcd5525e
0hj1hyeon <ojh010911@gmail.com>
2025-10-19 11:17:07 +0900

    feat: 컨트롤러 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.controller.CalController;
+import calculator.domain.Calculator;
+import calculator.domain.DelimiterManager;
+import calculator.view.IOView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        DelimiterManager delimiterManager = new DelimiterManager();
+        Calculator calculator = new Calculator(delimiterManager);
+
+        IOView view = new IOView();
+
+        CalController controller = new CalController(view, calculator);
+
+        controller.run();
     }
 }
